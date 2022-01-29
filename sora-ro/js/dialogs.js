965059ab5851fe49f934bf730ed113cd4cb2438d
sizhongbin <sizhongbin@hotmail.com>
2022-01-29 10:43:42 +0800

    开发同步

--- sora-ro/js/dialogs.js
+++ sora-ro/js/dialogs.js
@@ -4,18 +4,10 @@
  * */
 import { DEBUG } from './debug.js'
 
-/*
- * Modal
- *
- * Pico.css - https://picocss.com
- * Copyright 2019-2021 - Licensed under MIT
- */
-
-// 基础设置
-const isOpenClass = 'modal-is-open';
-const openingClass = 'modal-is-opening';
-const closingClass = 'modal-is-closing';
+// 动画时间
 const animationDuration = 400;
+
+// 当前展开的Modal
 let $visibleModal = null;
 
 // Modal开关
@@ -34,14 +26,10 @@ const isModalOpen = $modal => {
 
 // 开启Modal
 const openModal = $modal => {
-  if (isScrollbarVisible()) {
-  //document.documentElement.style.setProperty('--scrollbar-width', `${getScrollbarWidth()}px`);
-  $modal.find('#dialog-body').css('--scrollbar-width', `${getScrollbarWidth()}px`);
-  }
-  $modal.addClass(isOpenClass, openingClass);
+  $('html').addClass('modal-is-open modal-is-opening');
   setTimeout(() => {
     $visibleModal = $modal;
-    $modal.removeClass(openingClass);
+    $('html').removeClass('modal-is-opening');
   }, animationDuration);
   $modal.attr('open', true);
 }
@@ -49,49 +37,12 @@ const openModal = $modal => {
 // 关闭Modal
 const closeModal = $modal => {
   $visibleModal = null;
-  $modal.addClass(closingClass);
+  $('html').addClass('modal-is-closing');
   setTimeout(() => {
-    $modal.removeClass(closingClass, isOpenClass);
-    $modal.find('#dialog-body').css('--scrollbar-width','');
+    $('html').removeClass('modal-is-closing modal-is-open');
+    $('html').css('--scrollbar-width', '');
     $modal.removeAttr('open');
   }, animationDuration);
-}
-
-// 点击Modal外区域关闭
-document.addEventListener('click', event => {
-  if ($visibleModal != null) {
-    const $modalContent = $visibleModal.find('article');
-    const isClickInside = $.contains($modalContent, event.target);
-    !isClickInside && closeModal($visibleModal);
-  }
-});
-
-// 获取滚动条宽
-const getScrollbarWidth = () => {
-
-  // Creating invisible container
-  const outer = document.createElement('div');
-  outer.style.visibility = 'hidden';
-  outer.style.overflow = 'scroll'; // forcing scrollbar to appear
-  outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
-  document.body.appendChild(outer);
-
-  // Creating inner element and placing it in the container
-  const inner = document.createElement('div');
-  outer.appendChild(inner);
-
-  // Calculating difference between container's full width and the child width
-  const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
-
-  // Removing temporary elements from the DOM
-  outer.parentNode.removeChild(outer);
-
-  return scrollbarWidth;
-}
-
-// 判断对话框内容滚动条是否展示
-const isScrollbarVisible = () => {
-  return document.body.scrollHeight > screen.height;
 }
 
 /* Object常量
@@ -117,10 +68,13 @@ export const Dialogs = {
       let template =
         '<dialog id="dialog-update-log">' +
         '<article class="large-dialog flex-column-layout">' +
-        '<header>更新日志<ins id="dialog-update-log-close" class="dialog-close">X</ins></header>' +
+        '<header>更新日志</header>' +
         '<div id="dialog-body" class="flex-size">' +
-        '<p>开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中开发中</p>' +
+        '<p>开发中</p>' +
         '</div>' +
+        '<footer>' +
+        '<p id="dialog-update-log-close" role="button" class="outline">关闭</p>' +
+        '</footer>' +
         '</article>' +
         '</dialog>';
       if (DEBUG) console.log('return: ' + template);
